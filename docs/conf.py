1fdb24495faa73cf94169bf5f23350ce4e3e2fba
richter <richter@gfz-potsdam.de>
2013-11-04 18:37:36 +0100

    sphinx fix4

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.join(*os.path.abspath(__file__).split('/')[:-2]))
+sys.path.insert(0, '/var/build/user_builds/rf/envs/latest/')
+
 for p in sys.path:
     print p
 
