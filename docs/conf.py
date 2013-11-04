9a54ccb5a2bd0fe795ba1c528320ecdff90fc480
richter <richter@gfz-potsdam.de>
2013-11-04 18:20:22 +0100

    shinx fix

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.join(os.path.abspath(__file__).split('/')[:-1]))
 print sys.path
 
 class Mock(object):
@@ -38,6 +38,7 @@ class Mock(object):
             return Mock()
 
 MOCK_MODULES = ['numpy', 'obspy', 'obspy.core', 'obspy.core.util',
+                'obspy.core.util.geodetics',
                 'toeplitz', 'scipy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
