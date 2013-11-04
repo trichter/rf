f8552df20da84c7297dec5a2a8005bf8cf408b72
richter <richter@gfz-potsdam.de>
2013-11-04 18:56:21 +0100

    sphinx fix9

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     print 'SUC'
 else:
