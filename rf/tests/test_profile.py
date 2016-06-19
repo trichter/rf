460ee08fd6fdd6940727bae8be88404e85cd86c6
trichter <tom.eulenfeld@bgr.de>
2016-06-19 02:45:55 +0200

    skip profile test without cartopy

--- rf/tests/test_profile.py
+++ rf/tests/test_profile.py
@@ -8,9 +8,15 @@ from rf.profile import get_profile_boxes
 from rf.tests.test_rfstream import test_io_header
 from rf.util import minimal_example_rf
 
+try:
+    import cartopy
+except ImportError:
+    cartopy = None
+
 
 class ProfileTestCase(unittest.TestCase):
 
+    @unittest.skipIf(cartopy is None, 'cartopy not installed')
     def test_profile(self):
         dx = np.linspace(-50, 50, 41)
         boxes = get_profile_boxes((-21, -69.5), 85, dx, width=300)
@@ -18,7 +24,7 @@ class ProfileTestCase(unittest.TestCase):
         stream.extend(stream[:3])  # to actually stack something
         profile = stream.select(component='Q').profile(boxes)
         self.assertEqual(len(profile), 3)
-        str_ = 'profile | -5.0s - 22.0s | -6.25km slow: 6.40 (Ps moveout)'
+        str_ = 'profile | -10.0s - 80.0s | -6.25km slow:6.40 (Ps moveout)'
         self.assertEqual(str(profile[0]), str_)
         test_io_header(self, profile[:1])
 
