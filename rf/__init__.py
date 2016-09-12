dcfc2dac7b3fb22a4862fff42aaacbe88ee15137
trichter <tom.eulenfeld@bgr.de>
2016-09-12 14:00:23 +0200

    release new version

--- rf/__init__.py
+++ rf/__init__.py
@@ -214,7 +214,7 @@ version is |buildstatus|.
    :target: https://travis-ci.org/trichter/rf
 """
 
-__version__ = '0.4.2-dev'
+__version__ = '0.4.2'
 
 from rf.profile import get_profile_boxes
 from rf.rfstream import read_rf, RFStream, rfstats
