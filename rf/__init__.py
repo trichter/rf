f21014967db9fa18bd16a257e2a1a02289535d8f
trichter <tom.eulenfeld@bgr.de>
2017-04-11 11:23:34 +0200

    raise version number

--- rf/__init__.py
+++ rf/__init__.py
@@ -215,7 +215,7 @@ version is |buildstatus|.
    :target: https://travis-ci.org/trichter/rf
 """
 
-__version__ = '0.5.0'
+__version__ = '0.5.1-dev'
 
 from rf.profile import get_profile_boxes
 from rf.rfstream import read_rf, RFStream, rfstats
