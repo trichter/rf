26772bc2f826e04a0c0dcedc4ddca81c6ad19bd6
trichter <tom.eulenfeld@uni-jena.de>
2019-04-17 18:24:32 +0200

    add trove classifiers for python 3.7 and 3.8, version 0.7.0

--- rf/__init__.py
+++ rf/__init__.py
@@ -241,7 +241,7 @@ version is |buildstatus|.
    :target: https://travis-ci.org/trichter/rf
 """
 
-__version__ = '0.6.4-dev'
+__version__ = '0.7.0'
 
 from rf.profile import get_profile_boxes
 from rf.rfstream import read_rf, RFStream, rfstats
