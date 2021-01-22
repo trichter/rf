920507c24121142ac92e0d519117ac68a02069cb
trichter <tom.eulenfeld@uni-jena.de>
2021-01-22 18:17:54 +0100

    update supported python versions

--- setup.py
+++ setup.py
@@ -42,12 +42,9 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Scientific/Engineering :: Physics'
     ]
 
