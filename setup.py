efe67fae8d16b9341d31e9e5738d731d7711cbd9
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2024-04-11 14:12:27 +0200

    update python trove classifiers

--- setup.py
+++ setup.py
@@ -42,10 +42,6 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering :: Physics'
     ]
 
