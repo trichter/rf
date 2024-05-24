18d17af2b1ef84c4f75514074847e252d2e42f6b
trichter <tom.eulenfeld@uni-jena.de>
2024-05-24 23:01:01 +0200

    increase version number

--- rf/__init__.py
+++ rf/__init__.py
@@ -272,7 +272,7 @@ Tom Eulenfeld (2020), rf: Receiver function calculation in seismology, *Journal 
 .. _GitHub: https://github.com/trichter/rf/
 """
 
-__version__ = '1.1.0'
+__version__ = '1.1.1-dev'
 
 from rf.profile import get_profile_boxes
 from rf.rfstream import read_rf, RFStream, rfstats
